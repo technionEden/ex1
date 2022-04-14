5858c853175e66d610cf7cdd15c9515614da6608
Eden Bar-El <eden.bar@csl3>
2022-04-14 14:02:03 +0300

    Implemented Functions 1,2,3

--- RLEList.c
+++ RLEList.c
@@ -1,10 +1,73 @@
 #include "RLEList.h"
 
+
+int main() {
+	RLEList head = RLEListCreate();
+    RLEListDestroy(head);
+
+	return 0;
+}
+
+
+
+
 struct RLEList_t{
-    //TODO: implement
+    int amount;
+    char letter;
+    struct RLEList_t* next;
 };
 
 //implement the functions here
 
+RLEList RLEListCreate()
+{
+    RLEList ptr = malloc(sizeof(*ptr));
+	if(!ptr) {
+		return NULL;
+	}
+	ptr->amount = 0;
+    ptr->letter = 'A';
+	ptr->next = NULL;
+	return ptr;
+}
 
-// EDITED
+void RLEListDestroy(RLEList list)
+{
+    while(list) {
+		RLEList toDelete = list;
+		list = list->next;
+		free(toDelete);
+	}
+}
+
+
+RLEListResult RLEListAppend(RLEList list, char value)
+{
+    if (!list || !value) {
+        return RLE_LIST_NULL_ARGUMENT;
+    }
+    RLEList ptr = list;
+    while (ptr->next) {
+        ptr = ptr->next;
+    }
+
+    // SAME CHAR AS CURRENT NODE
+
+    if (ptr->letter == value) {
+        ptr->amount = ptr->amount+1;
+    }
+    // DIFFERENT CHAR FROM CURRENT NODE
+    
+    else {
+        RLEList newNode = RLEListCreate();
+        if (!newNode){
+            return RLE_LIST_OUT_OF_MEMORY;
+        }
+        ptr->next = newNode;
+        newNode->amount = 1;
+        newNode->letter = value;
+    }
+
+    return RLE_LIST_SUCCESS;
+
+}
--- RLEList.h
+++ RLEList.h
@@ -6,6 +6,7 @@
 #define HW1_RLELIST_H
 
 #include <stdio.h>
+#include <stdlib.h>
 #include <stdbool.h>
 
 /**
