3d5de3b1404feb8d607f4e697a197e009ed1dff4
Nevo Harel <nevoharel07@gmail.com>
2022-04-23 23:04:33 +0300

    Trial

--- RLEList.c
+++ RLEList.c
@@ -3,7 +3,7 @@
 #include <stdlib.h>
 
 /**
-*   RLEListAppend_aux: adds value to list. 
+*   RLEListAppend_aux: Aadds value to list. 
 *
 * @param finalPtr - The final node in the list.
 * @param value - The character which needs to be added.
